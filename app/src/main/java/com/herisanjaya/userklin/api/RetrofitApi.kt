3c3dfff60177a6493d128f2028e26c99b7b0e795
Heri Sanjaya Ginting <arsip.herisanjayaginting@gmail.com>
2023-12-11 12:48:12 +0700

    created retrofit interface

---
+++ app/src/main/java/com/herisanjaya/userklin/api/RetrofitApi.kt
@@ -0,0 +1,69 @@
+package com.herisanjaya.userklin.api
+
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Headers
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Part
+import retrofit2.http.Path
+
+interface RetrofitApi {
+    @GET("/")
+    fun getAll(@Header("X-API-Key") apiKey: String): Call<ResponseBody>
+
+    @DELETE("/")
+    fun deleteAll(@Header("X-API-Key") apiKey: String): Call<ResponseBody>
+
+    @GET("/users")
+    fun getUsers(@Header("X-API-Key") apiKey: String): Call<ResponseBody>
+
+    @GET("/users/{id}")
+    fun getUserById(@Header("X-API-Key") apiKey: String, @Path("id") idUser: String): Call<ResponseBody>
+
+    @POST("/users")
+    @Multipart
+    fun createUser(
+        @Header("X-API-Key") apiKey: String,
+        @Part("users_name") userName: RequestBody,
+        @Part("users_email") userEmail: RequestBody,
+        @Part("users_password") userPassword: RequestBody,
+        @Part("users_phone") userPhone: RequestBody,
+        @Part("users_role") userRole: RequestBody,
+        @Part userPicture: MultipartBody.Part
+    ): Call<ResponseBody>
+
+    @PUT("/users/{id}")
+    @Multipart
+    fun updateUser(
+        @Header("X-API-Key") apiKey: String,
+        @Path("id") idUser: String,
+        @Part("users_name") userName: RequestBody,
+        @Part("users_email") userEmail: RequestBody,
+        @Part("users_phone") userPhone: RequestBody,
+        @Part("users_role") userRole: RequestBody,
+        @Part userPicture: MultipartBody.Part
+    ): Call<ResponseBody>
+
+    @DELETE("/users/{id}")
+    fun deleteUser(@Header("X-API-Key") apiKey: String, @Path("id") idUser: String): Call<ResponseBody>
+
+    @GET("/laundry")
+    fun getLaundries(@Header("X-API-Key") apiKey: String): Call<ResponseBody>
+
+    @POST("/predict")
+    @Multipart
+    fun predictImage(@Header("X-API-Key") apiKey: String, @Part image: MultipartBody.Part): Call<ResponseBody>
+
+    @POST("/signup")
+    @Headers("Content-Type: application/json")
+    fun signUp(@Header("x-api-key") apiKey: String, @Body body: RequestBody): Call<ResponseBody>
+
+}
