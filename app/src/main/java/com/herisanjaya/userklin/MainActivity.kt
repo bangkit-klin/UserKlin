2810b052b1952c5ea8b03653304c51772082f843
Heri Sanjaya Ginting <arsip.herisanjayaginting@gmail.com>
2023-12-11 16:54:38 +0700

    create serverdata handler

--- app/src/main/java/com/herisanjaya/userklin/MainActivity.kt
+++ app/src/main/java/com/herisanjaya/userklin/MainActivity.kt
@@ -2,10 +2,14 @@ package com.herisanjaya.userklin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.herisanjaya.userklin.api.ServerData
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val test = ServerData()
+        test.test()
     }
 }
--- app/src/main/java/com/herisanjaya/userklin/api/ServerData.kt
+++ app/src/main/java/com/herisanjaya/userklin/api/ServerData.kt
@@ -1,5 +1,9 @@
 package com.herisanjaya.userklin.api
 
+import android.util.Log
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -7,7 +11,8 @@ class ServerData {
 
     companion object {
 //        tambahkan base url
-        private const val BASE_URL = ""
+        private const val BASE_URL = "https://klin-server-backend-bhvoy6ma5a-et.a.run.app"
+        private const val API_KEY = "inkubasi2023"
     }
 
     private fun getInstance(): RetrofitApi {
@@ -18,8 +23,51 @@ class ServerData {
         return retrofit
     }
 
-    private fun getAllUsers() {
-
+    public fun test() {
+        getAllUsers()
+        getUser("u1701666978475")
     }
+
+    private fun getAllUsers() {
+        val call = getInstance().getUsers(API_KEY)
+        call.enqueue(object : Callback<ArrayList<User>> {
+            override fun onResponse(
+                call: Call<ArrayList<User>>,
+                response: Response<ArrayList<User>>
+            ) {
+                val listUser = response.body()
+                if(listUser == null) {
+                    Log.d("list_users_success", "NULL RESPOND")
+                } else {
+                    Log.d("list_users_success", listUser.toString())
+                }
+            }
+
+            override fun onFailure(call: Call<ArrayList<User>>, t: Throwable) {
+                t.message?.let { Log.d("list_users_error", it) }
+            }
+
+        })
+    }
+
+    private fun getUser(id: String) {
+        val call = getInstance().getUserById(API_KEY, id)
+        call.enqueue(object : Callback<User> {
+            override fun onResponse(call: Call<User>, response: Response<User>) {
+                val user = response.body()
+                if(user == null) {
+                    Log.d("get_user_success", "NULL RESPOND")
+                } else {
+                    Log.d("get_user_success", user.toString())
+                }
+            }
+
+            override fun onFailure(call: Call<User>, t: Throwable) {
+                t?.message?.let { Log.d("get_user_failure", it) }
+            }
+        })
+    }
+
+
 
 }
